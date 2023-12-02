bcc459178ba5eee8aee1dbe8f8dc9f0de18793e7
G-Chandu-Reddy <77720225+G-Chandu-Reddy@users.noreply.github.com>
2023-12-03 00:12:01 +0530

    Delete password-generator-be directory

--- password-generator-be/src/main/java/com/champ/techie/passwordmanager/repository/UserDetailsRepository.java
+++
@@ -1,7 +0,0 @@
-package com.champ.techie.passwordmanager.repository;
-
-import com.champ.techie.passwordmanager.model.UserDetails;
-import org.springframework.data.repository.CrudRepository;
-
-public interface UserDetailsRepository extends CrudRepository<UserDetails, String> {
-}
